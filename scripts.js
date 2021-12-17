499393b44bd2e1bcea81129f5f6b3f046cd8a829
jeancs21 <40588173+jeancs21@users.noreply.github.com>
2021-12-17 19:46:16 -0400

    switch button agregado con su funcion. Responsive para la pagina y un transform a la columna del medio.

--- scripts.js
+++ scripts.js
@@ -2,17 +2,15 @@ function tooglePrice(){
     const basicPrice = document.getElementById("basic-price");
     const professionalPrice = document.getElementById("professional-price");
     const masterPrice = document.getElementById("master-price");
-    const boton = document.getElementById("btnPrice");
-    if (boton.textContent === "Anually") {
-        boton.textContent = "Monthly";
-        basicPrice.textContent = "$199.99";
-        professionalPrice.textContent = "$249.99";
-        masterPrice.textContent = "$399.99";
-    }
-    else {
-        boton.textContent = "Anually";
+    const chk = document.getElementById("checkInput");
+    if (chk.checked == true) {
         basicPrice.textContent = "$19.99";
         professionalPrice.textContent = "$24.99";
         masterPrice.textContent = "$39.99";
+    }
+    else {
+        basicPrice.textContent = "$199.99";
+        professionalPrice.textContent = "$249.99";
+        masterPrice.textContent = "$399.99";
     }
 }
